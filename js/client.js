9663541fb6d9484ef593dbf597d584e23a90d1c7
Nichlas <nh@jappes.dk>
2015-10-29 11:13:26 +0100

    creating repost

---
+++ js/client.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+   $("h1").css("color", "green")
+    console.log("console is ready")
+     $("#button").click(function() {
+         console.log("Button Clicked");
+
+     });
+});
