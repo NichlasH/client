b588b13dabeff124bc250e757bfd082e2e828724
Nichlas <nh@jappes.dk>
2015-11-11 12:08:05 +0100

    Coming soon for signup added

--- js/client.js
+++ js/client.js
@@ -16,14 +16,15 @@ $("#loginform").submit(function(e) {
         data: data, // serializes the form's elements.
         error: function(response) {
             $(".info").text("Unfortunately the server is not responding").fadeTo(1000, 1).fadeTo(2000, 0);
-           alert("Something went wrong");
+           //for test purposes
+            window.location.href = 'game.html';
         },
         success: function(response)
         {
             console.log(response.message); // show response
             if(response.userid) {
                 window.location.href = 'game.html';
-                console.log("Du er logget ind")
+
                             }
             else {
                 $(".info").text("Wrong username or password. Please try again.").fadeTo(1000, 1).fadeTo(2000, 0);
