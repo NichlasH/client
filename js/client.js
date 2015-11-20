cd4ea43d458de9879c5f62fba3b4880dfc424b67
Nichlas <nh@jappes.dk>
2015-11-20 14:16:35 +0100

    score table created via client.js

--- js/client.js
+++ js/client.js
@@ -4,9 +4,12 @@ $("#loginform").submit(function(e) {
 
     var Username = $('#userfrm').val();
     var Password = $('#passfrm').val();
-
+    var user = {
+        username: Username,
+        password: Password
+    };
     var url = "http://localhost:9998/api/login/"; // the script where you handle the form input.
-    var data = '{"username": "' + Username + '", "password": "'+ Password +'"}';
+    var data = JSON.stringify(user);
 
     console.log(data);
     $.ajax({
@@ -35,6 +38,90 @@ $("#loginform").submit(function(e) {
 
     e.preventDefault(); // avoid to execute the actual submit of the form.
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+
+    var url = "http://localhost:9998/api/scores/"; // the script where you handle the form input.
+    var display = {};
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: "JSONP",
+        success: function () {
+            console.log()
+        },
+        error: function (e) {
+            //called when there is an error
+            //console.log(e.message);
+        }
+    });
+});
+
+
+
+    $.get("http://localhost:9998/api/scores", function( data )  {
+        //$( ".result" ).html( data );
+        for (var i=0;i<data.length;++i)
+        {
+            $('#pending tr:last').after('<tr>' +
+                '<td>' + (i+1) + '</td>' +
+                '<td>' + data[i].user.firstName +" "+ data[i].user.lastName + '</td>' +
+                '<td>' + data[i].score + '</td>' +
+
+                '</tr>');
+        }
+console.log(data)
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
